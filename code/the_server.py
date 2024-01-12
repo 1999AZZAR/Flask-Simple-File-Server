41db4e94adc1b0d2843d64d4e74934a15b568b31
azzar budiyanto <azzar.mr.zs@gmail.com>
2024-01-13 06:23:36 +0700

    ..
    A slight modification to add a touch of sophistication, making it slightly more robust than before.

--- code/the_server.py
+++ code/the_server.py
@@ -8,7 +8,7 @@ import logging
 app = Flask(__name__)
 CORS(app)
 
-BASE_FOLDER = '/media/azzar/betha/Downloads/project/quick_count/databased/'
+BASE_FOLDER = '/path/to/where/you/want/to/save/the/data/'
 OTHER_FOLDER = 'other_files'
 XML_FOLDER = 'xml_files'
 IMG_FOLDER = 'img_files'
